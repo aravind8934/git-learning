c7de318e5f2ac5ed1769c15508d5c95043667618
aravind <aravindh8934@gmail.com>
2023-08-26 23:36:35 -0400

    just modified

--- get_vpc_id.sh
+++ get_vpc_id.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VPCID=$(aws ec2 describe-vpcs --region $1 | jq ".vpcs[].VpcId" -r)
 for VPC in $VPCID; do
-echo "-----------------------------"
-echo "The vpc id is $VPC"
-echo "-----------------------------"
+    echo "-----------------------------"
+    echo "The vpc id is $VPC"
+    echo "-----------------------------"
 done
