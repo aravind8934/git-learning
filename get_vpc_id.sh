106a1c557f3f38831be7da4d640396102cfd9162
aravind <aravindh8934@gmail.com>
2023-08-27 00:54:56 -0400

    added $123

--- get_vpc_id.sh
+++ get_vpc_id.sh
@@ -5,6 +5,15 @@ for VPC in $VPCID; do
     echo "The vpc id is $VPC"
     echo "-----------------------------"
 done
-
-echo $0
-echo $1
+VPCID=$(aws ec2 describe-vpcs --region $2 | jq ".vpcs[].VpcId" -r)
+for VPC in $VPCID; do
+    echo "-----------------------------"
+    echo "The vpc id is $VPC"
+    echo "-----------------------------"
+done
+VPCID=$(aws ec2 describe-vpcs --region $3 | jq ".vpcs[].VpcId" -r)
+for VPC in $VPCID; do
+    echo "-----------------------------"
+    echo "The vpc id is $VPC"
+    echo "-----------------------------"
+done
